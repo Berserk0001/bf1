777a5c354b3e6aaa989b90941fe3c8db1b47ad40
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-06-29 09:06:08 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,5 +1,5 @@
 const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desired
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 2;
 
 function shouldCompress(req) {
   const { originType, originSize, webp } = req.params;
