8bd10e17c3488536a132b424aea1513cfaea9880
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-08-02 16:20:30 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -2,7 +2,7 @@ const MIN_COMPRESS_LENGTH = 1024; // Adjust the minimum compress length as desir
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 10;
 
 function shouldCompress(req) {
-  const { originType, originSize, avif } = req.params;
+  const { originType, originSize, heif } = req.params;
 
   if (!originType.startsWith('image')) {
     return false;
@@ -10,10 +10,10 @@ function shouldCompress(req) {
   if (originSize === 0) {
     return false;
   }
-  if (avif && originSize < MIN_COMPRESS_LENGTH) {
+  if (heif && originSize < MIN_COMPRESS_LENGTH) {
     return false;
   }
-  if (!avif && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
+  if (!heif && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
     return false;
   }
 
