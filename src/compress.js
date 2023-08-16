592b4bc71a5680f704f80db23060bd83ebcce6e2
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-08-16 12:47:16 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -3,7 +3,7 @@ const redirect = require('./redirect');
 
 function compress(req, res, input) {
   const format = req.params.avif ? 'avif' : 'jpeg';
-  let compressionQuality = req.params.quality * 0.01;
+  let compressionQuality = req.params.quality * 0.1;
        
   req.params.quality = Math.ceil(compressionQuality);
 
